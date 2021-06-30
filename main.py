ef3501881a1c63608dfdf24ff968af7f7338a2b0
JoaoSironi <joaovsironi@gmail.com>
2021-06-29 22:56:16 -0300

    finishing the visual response

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import time
 import random
+import os
+from colorama import Fore, init
+init(autoreset=True)
+
+def menu():
+    print(f'#############################################################')
+    print(f'#                    Welcome to {Fore.LIGHTRED_EX}Jokenpo{Fore.WHITE}                     #')
+    print(f'# Stone wins Scissors, Scissors wins Paper, Paper wins Rock #')
+    print(f'#                    Write quit to close                    #')
+    print(f'#############################################################')
 
 def userChoice():
     choice = ''
     while choice not in ('rock', 'paper', 'scissors'):
-        choice = input('Make your choice: ').lower()
+        choice = input('Make your choice(Rock, Paper or Scissors): ').lower()
         if choice == 'quit':
             print('End game')
-            time.sleep(3)
+            time.sleep(2)
             break
         elif choice not in ('rock', 'paper', 'scissors'):
             print('Sorry, this value is not valid')
@@ -21,36 +31,44 @@ def botChoice():
 def mechanics(user, bot):
 
     if (user == 'rock' and bot == 'scissors') or (user == 'paper' and bot == 'rock') or (user == 'scissors' and bot == 'paper'):
-        print('Nice you won!!')
-        print(f'You chose {user}')
-        print(f'Bot chose {bot}')
-        print('We are the champions my friend')
-        time.sleep(5)
-    elif (bot == 'rock' and user == 'scissors') or (bot == 'paper' and user == 'rock') or (bot == 'scissors' and user == 'paper'):
-        print('looooooser!!')
-        print(f'You chose {user}')
-        print(f'Bot chose {bot}')
-        print('Try again...')
+        print(f'You chose {Fore.CYAN}{user}')
         time.sleep(1)
-        print('in another life')
-        time.sleep(4)
+        print(f'Bot chose {Fore.LIGHTBLUE_EX}{bot}')
+        time.sleep(1)
+        print(f'{Fore.GREEN}Nice you won!!')
+        print('We are the champions my friend')
+        time.sleep(2)
+    elif (bot == 'rock' and user == 'scissors') or (bot == 'paper' and user == 'rock') or (bot == 'scissors' and user == 'paper'):
+        print(f'You chose {Fore.CYAN}{user}')
+        time.sleep(1)
+        print(f'Bot chose {Fore.LIGHTBLUE_EX}{bot}')
+        time.sleep(1)
+        print(f'{Fore.RED}looooooser!!')
+        print('Try again...')
+        time.sleep(2)
     else:
-        print('Tie, try again')
-        print(f'You chose {user}')
-        print(f'Bot chose {bot}')
-        time.sleep(5)
+        print(f'You chose {Fore.CYAN}{user}')
+        time.sleep(1)
+        print(f'Bot chose {Fore.LIGHTBLUE_EX}{bot}')
+        time.sleep(1)
+        print(f'{Fore.YELLOW}Tie, try again')
+        time.sleep(2)
 
 def clearScreen():
-    print()
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
 def main():
     quit = False
     while quit == False:
+        clearScreen()
+        menu()
         user = userChoice()
         if user == 'quit':  
             break
         bot = botChoice()
         mechanics(user, bot)
-        clearScreen()
 
 main()
